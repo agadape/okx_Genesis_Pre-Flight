7484bb62a2c4bad81e4d816f1e2135735b60d01f
agadape <agadape@gmail.com>
2026-07-11 20:58:38 +0700

    fix: suppress TS error for okx sdk subpath import to fix vercel build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { paymentMiddleware, x402ResourceServer } from "@okxweb3/x402-express";
+// @ts-ignore
 import { ExactEvmScheme } from "@okxweb3/x402-evm/exact/server.js";
 import { OKXFacilitatorClient } from "@okxweb3/x402-core";
 import { config } from "./config.js";
